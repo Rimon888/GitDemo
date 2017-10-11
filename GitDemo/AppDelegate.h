53df1707e1d7bfd74465f0e7fef20dfc64410d19
wangweike <wangweike@wangweikedeMacBook-Air.local>
2017-10-11 11:15:39 +0800

    initial commit

---
+++ GitDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitDemo
+//
+//  Created by wangweike on 2017/10/11.
+//  Copyright © 2017年 zlm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GitDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitDemo
+//
+//  Created by wangweike on 2017/10/11.
+//  Copyright © 2017年 zlm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
